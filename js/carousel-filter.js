fe79580520ff64a70f17cfc7d4fe90dfd2e63b78
Lance Andrei Sombillo <lancesombillo11@gmail.com>
2026-01-05 17:35:53 +0800

    Modified some measurement from elements, for a better Mobile UI/UX and responsiveness. Added filtering feature for the project gallery using JavaScript and added Hyperlinks for direct Contact. Will add project next.

---
+++ js/carousel-filter.js
@@ -0,0 +1,64 @@
+const showAll = document.getElementById("show-all");
+const showSoftware = document.getElementById("show-sw-only");
+const sw_card = document.querySelectorAll(".software");
+const showHardware = document.getElementById("show-hw-only");
+const hw_card = document.querySelectorAll(".hardware");
+
+let filter_option = 'all';
+
+
+
+function applyFilter(option) {
+    switch (option) {
+        case 'all':
+            showAll.style.backgroundColor = 'var(--card-button-hover-background-color)';
+            showSoftware.style.backgroundColor = 'var(--card-button-background-color)';
+            showHardware.style.backgroundColor = 'var(--card-button-background-color)';
+            sw_card.forEach(item => {
+                item.style.display = 'block';
+            });
+            hw_card.forEach(item => {
+                item.style.display = 'block';
+            });
+            break;
+
+        case 'software':
+            showSoftware.style.backgroundColor = 'var(--card-button-hover-background-color)';
+            showAll.style.backgroundColor = 'var(--card-button-background-color)';
+            showHardware.style.backgroundColor = 'var(--card-button-background-color)';
+            sw_card.forEach(item => {
+                item.style.display = 'block';
+            });
+            hw_card.forEach(item => {
+                item.style.display = "none";
+            });
+            break;
+
+        case 'hardware':
+            showHardware.style.backgroundColor = 'var(--card-button-hover-background-color)';
+            showSoftware.style.backgroundColor = 'var(--card-button-background-color)';
+            showAll.style.backgroundColor = 'var(--card-button-background-color)';
+            hw_card.forEach(item => {
+                item.style.display = 'block';
+            });
+            sw_card.forEach(item => {
+                item.style.display = "none";
+            });
+            break;
+    }
+}
+
+applyFilter(filter_option);
+
+showAll.onclick = function () {
+    filter_option = 'all';
+    applyFilter(filter_option);
+}
+showSoftware.onclick = function () {
+    filter_option = 'software';
+    applyFilter(filter_option);
+}
+showHardware.onclick = function () {
+    filter_option = 'hardware';
+    applyFilter(filter_option);
+}
